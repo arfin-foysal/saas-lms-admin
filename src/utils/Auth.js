f02684148a197f9f6b962e034ed3b36e96b6b87f
arfin foysal <arfinfoysalbd@gmail.com>
2023-07-13 17:04:02 +0600

    pwa problem fixed

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,11 +1,11 @@
 import Cookies from "js-cookie";
-const role = JSON.parse(localStorage.getItem("lms_user_role"));
-
-export const authUser = localStorage.getItem("lms_user_role")
-  ? role
+export const authUserToken = Cookies.get("lms_token") ? "token" : "";
+// const role = JSON.parse(localStorage.getItem("lms_user_role"));
+export const authUser = JSON.parse(localStorage.getItem("lms_user_role"))
+  ? JSON.parse(localStorage.getItem("lms_user_role"))
   : "";
 
-export const authUserToken = Cookies.get("lms_token") ? "token" : "";
+
 
 
 
