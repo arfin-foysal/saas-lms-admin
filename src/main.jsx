0c23984cd28b8fca2e79d0d6ee6ccaa6d721336e
arfin foysal <arfinfoysalbd@gmail.com>
2023-07-30 17:04:22 +0600

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -8,19 +8,15 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {ToastContainer} from "react-toastify";
 import store from "./store/index";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")
 ).render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <DevSupport ComponentPreviews={ComponentPreviews}
-                            useInitialHook={useInitial}
-                >
+           
                     <App/>
-                </DevSupport>
+          
                 <ToastContainer/>
             </BrowserRouter>
         </Provider>
