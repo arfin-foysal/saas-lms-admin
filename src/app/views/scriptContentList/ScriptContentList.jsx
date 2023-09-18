207373bf4f1b5d8590b398384543ab1723258852
arfin foysal <arfinfoysalbd@gmail.com>
2023-09-18 11:39:36 +0600

    script filter bug fixing

--- src/app/views/scriptContentList/ScriptContentList.jsx
+++ src/app/views/scriptContentList/ScriptContentList.jsx
@@ -10,39 +10,62 @@ import { BsArrowRightShort, BsEyeFill } from "react-icons/bs";
 import { useGetChapterListBySubjectIdQuery, useGetClassListQuery, useGetScriptChapterListQuery, useGetSubjectListByClassIdQuery } from "../../../services/contentApi";
 import Select from "react-select";
 import { BiReset } from "react-icons/bi";
+import { useFormik } from "formik";
 
 const ScriptContentList = () => {
   const [clickValue, setClickValue] = useState(null);
   const [param, setParam] = useState(null);
   const [show, setShow] = useState(false);
-  const [classId, setClassId] = useState(0);
-  const [subjectId, setSubjectId] = useState(0);
-  const [chapterId, setChapterId] = useState(0);
-
-  const classRes = useGetClassListQuery();
-  const subjectRes = useGetSubjectListByClassIdQuery(classId?.id ? classId?.id : 0);
-  const chapterRes = useGetChapterListBySubjectIdQuery(subjectId?.id ? subjectId?.id : 0);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const reFetch = () => {
-    res.refetch();
-    setClassId(0);
-    setSubjectId(0);
-    setChapterId(0);
-  };
-
-  const res = useGetScriptChapterListQuery({
-    class_id: classId?.id ? classId?.id : 0,
-    subject_id: subjectId?.id ? subjectId?.id : 0,
-    chapter_id: chapterId?.id ? chapterId?.id : 0,
-  });
-
-
   const handelClickValue = useCallback((value) => {
     setClickValue(value);
   }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      classId: "",
+      subjectId: "",
+      chapterId: "",
+    },
+    onSubmit: async (values, { resetForm }) => {
+      res.refetch({
+        class_id: values.classId?.id ? values.classId?.id : 0,
+        subject_id: values.subjectId?.id ? values.subjectId?.id : 0,
+        chapter_id: values.chapterId?.id ? values.chapterId?.id : 0,
+      });
+    },
+  });
+
+  const classRes = useGetClassListQuery();
+  const subjectRes = useGetSubjectListByClassIdQuery(
+    formik.values.classId?.id ? formik.values.classId?.id : 0
+  );
+  const chapterRes = useGetChapterListBySubjectIdQuery(
+    formik.values.subjectId?.id ? formik.values.subjectId?.id : 0
+  );
+
+  const res = useGetScriptChapterListQuery({
+    class_id: formik.values.classId?.id ? formik.values.classId?.id : 0,
+    subject_id: formik.values.subjectId?.id ? formik.values.subjectId?.id : 0,
+    chapter_id: formik.values.chapterId?.id ? formik.values.chapterId?.id : 0,
+  });
+
+  const { data, isSuccess, isFetching, isError } = res;
+
+  const reFetch = () => {
+    res.refetch();
+    formik.setFieldValue('subjectId', '');
+    formik.setFieldValue('chapterId', '');
+  };
+
+  const handleChangeValue = (e) => {
+    formik.setFieldValue('subjectId', '');
+    formik.setFieldValue('chapterId', '');
+  }
+  const handleChangeValue1 = (e) => {
+    formik.setFieldValue('chapterId', '');
+  }
 
   const columns = useMemo(
     () => [
@@ -109,7 +132,6 @@ const ScriptContentList = () => {
     ],
     []
   );
-  const { data, isSuccess, isFetching, isError } = res;
 
   return (
     <>
@@ -121,138 +143,142 @@ const ScriptContentList = () => {
         paramValue={param}
       />
       <PageTopHeader title="Script List" />
-      <div className="card border shadow-lg ">
-        <div className="card-header d-flex justify-content-between ">
-          <div>Script List</div>
-          <div>
-            <button
-              className="btn btn-primary btn-sm"
-              onClick={() => {
-                handleShow();
-                handelClickValue("Add New Script");
+      <form
+        className="form-sample"
+        onSubmit={formik.handleSubmit}
+        encType="multipart/form-data"
+      >
+        <div className="card border shadow-lg ">
+          <div className="card-header d-flex justify-content-between ">
+            <div>Script List</div>
+            <div>
+              <button
+                className="btn btn-primary btn-sm"
+                onClick={() => {
+                  handleShow();
+                  handelClickValue("Add New Script");
+                }}
+              >
+                <FiPlusCircle size={16} /> Add New Script
+              </button>
+            </div>
+          </div>
+
+          <div className="card-body p-0">
+            <MaterialReactTable
+              renderTopToolbarCustomActions={() => (
+                <div className="col-md-6 gap-1 d-flex justify-content-start ">
+                  <Select
+                    className="w-100"
+                    isClearable
+                    menuPortalTarget={document.body}
+                    styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
+                    placeholder="Select Class"
+                    isLoading={classRes?.isFetching}
+                    onChange={(e) => {
+                      formik.setFieldValue("classId", e)
+                      handleChangeValue(e)
+                    }}
+                    getOptionLabel={option => option.name}
+                    getOptionValue={option => option.id}
+                    options={classRes?.data?.data}
+                    name="classId"
+                    value={formik.values.classId}
+                  />
+
+                  <Select
+                    className="w-100"
+                    isClearable
+                    menuPortalTarget={document.body}
+                    styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
+                    placeholder="Select Subject"
+                    isLoading={subjectRes?.isFetching}
+                    onChange={(e) => {
+                      formik.setFieldValue("subjectId", e)
+                      handleChangeValue1(e)
+                    }
+                    }
+                    getOptionLabel={option => option.name}
+                    getOptionValue={option => option.id}
+                    options={subjectRes?.data?.data}
+                    name="subjectId"
+                    value={formik.values.subjectId}
+
+                  />
+                  <Select
+                    className="w-100"
+                    isClearable
+                    menuPortalTarget={document.body}
+                    styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
+                    placeholder="Select Chapter"
+                    isLoading={chapterRes?.isFetching}
+                    onChange={(e) => {
+                      formik.setFieldValue("chapterId", e)
+                    }}
+                    getOptionLabel={option => option.name}
+                    getOptionValue={option => option.id}
+                    options={chapterRes?.data?.data}
+                    name="chapterId"
+                    value={formik.values.chapterId}
+                  />
+                  <div>
+                    <BiReset
+                      className="pointer  mt-2"
+                      color="white"
+                      size={25}
+                      onClick={reFetch}
+                    />
+                  </div>
+                </div>
+              )}
+              columns={columns}
+              data={isSuccess ? data?.data : []}
+              enableRowActions
+              enableColumnActions
+              positionActionsColumn="last"
+              muiTopToolbarProps={{
+                style: {
+                  backgroundColor: tableColor ? tableColor : "#0675F8",
+                },
               }}
-            >
-              <FiPlusCircle size={16} /> Add New Script
-            </button>
+              // enablePagination="true"
+              renderRowActions={(row, index) => (
+                <>
+                  <div className="d-flex">
+                    <button
+                      title=""
+                      className="px-2 mx-1 d-flex align-items-center btn btn-success btn-sm"
+                      onClick={() => {
+                        handleShow();
+                        handelClickValue("Update Script");
+                        setParam(row?.row?.original);
+                      }}
+                    >
+                      <FaEdit size={16} /> Edit
+                    </button>
+
+                    <button
+                      title=""
+                      className="px-2 mx-1 d-flex align-items-center btn btn-info btn-sm"
+                      onClick={() => {
+                        handleShow();
+                        handelClickValue("Script Details");
+                        setParam(row?.row?.original);
+                      }}
+                    >
+                      <BsEyeFill size={16} /> Details
+                    </button>
+                  </div>
+                  <div>
+
+                  </div>
+
+                </>
+              )}
+            />
           </div>
         </div>
-
-        <div className="card-body p-0">
-          <MaterialReactTable
-            renderTopToolbarCustomActions={() => (
-              <div className="col-md-6 gap-1 d-flex justify-content-start ">
-                <Select
-                  className="w-100"
-                  isClearable
-                  menuPortalTarget={document.body}
-                  styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
-                  placeholder="Select Class"
-                  isLoading={classRes?.isFetching}
-                  onChange={(e) => {
-                    setClassId(e)
-                    setSubjectId(0);
-                    setChapterId(0);
-                  }}
-                  getOptionValue={(option) => `${option["id"]}`}
-                  getOptionLabel={(option) => `${option["name"]}`}
-                  options={classRes?.data?.data}
-                  key={classRes?.data?.data?.id}
-                  name="class_id"
-                  value={classRes?.data?.data?.id}
-                  
-
-                />
-                <Select
-                  className="w-100"
-                  isClearable
-                  menuPortalTarget={document.body}
-                  styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
-                  placeholder="Select Subject"
-                  isLoading={subjectRes?.isFetching}
-                  onChange={(e) => {
-                    setSubjectId(e);
-                    setChapterId(0);
-                  }}
-                  getOptionValue={(option) => `${option["id"]}`}
-                  getOptionLabel={(option) => `${option["name"]}`}
-                  options={subjectRes?.data?.data}
-                  key={subjectRes?.data?.data?.id}
-                  name="subject_id"
-                  value={subjectRes?.data?.data?.id}
-                />
-                <Select
-                  className="w-100"
-                  isClearable
-                  menuPortalTarget={document.body}
-                  styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
-                  placeholder="Select Chapter"
-                  isLoading={chapterRes?.isFetching}
-                  onChange={(e) => setChapterId(e)}
-                  getOptionValue={(option) => `${option["id"]}`}
-                  getOptionLabel={(option) => `${option["name"]}`}
-                  options={chapterRes?.data?.data}
-                  name="chapter_id"
-                  key={chapterRes?.data?.data?.id}
-                  value={chapterRes?.data?.data?.id}
-                  
-                />
-                <div>
-                  <BiReset
-                    className="pointer  mt-2"
-                    color="white"
-                    size={25}
-                    onClick={reFetch}
-                  />
-                </div>
-              </div>
-            )}
-            columns={columns}
-            data={isSuccess ? data?.data : []}
-            enableRowActions
-            enableColumnActions
-            positionActionsColumn="last"
-            muiTopToolbarProps={{
-              style: {
-                backgroundColor: tableColor ? tableColor : "#0675F8",
-              },
-            }}
-            // enablePagination="true"
-            renderRowActions={(row, index) => (
-              <>
-                <div className="d-flex">
-                  <button
-                    title=""
-                    className="px-2 mx-1 d-flex align-items-center btn btn-success btn-sm"
-                    onClick={() => {
-                      handleShow();
-                      handelClickValue("Update Script");
-                      setParam(row?.row?.original);
-                    }}
-                  >
-                    <FaEdit size={16} /> Edit
-                  </button>
-
-                  <button
-                    title=""
-                    className="px-2 mx-1 d-flex align-items-center btn btn-info btn-sm"
-                    onClick={() => {
-                      handleShow();
-                      handelClickValue("Script Details");
-                      setParam(row?.row?.original);
-                    }}
-                  >
-                    <BsEyeFill size={16} /> Details
-                  </button>
-                </div>
-                <div>
-
-                </div>
-
-              </>
-            )}
-          />
-        </div>
-      </div>
+      </form>
     </>
   );
 };
